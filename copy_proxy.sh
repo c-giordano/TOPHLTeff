fbed8335f4ee059b02e36b31012570649c9f8972
cristina.giordano <cristina.giordano@cern.ch>
2025-11-20 13:48:38 +0100

    copy proxy sh

---
+++ copy_proxy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Step 1: Crea nuova proxy
+voms-proxy-init -voms cms -valid 192:00
+PROXY_SRC=$(voms-proxy-info -path)
+
+# Step 2: Destinazione persistente
+PROXY_DST="/afs/cern.ch/user/c/cgiordan/.x509up_u142167"
+
+# Step 3: Copia con permessi corretti
+cp "$PROXY_SRC" "$PROXY_DST"
+chmod 600 "$PROXY_DST"
+
+echo "Proxy copied to: $PROXY_DST"
